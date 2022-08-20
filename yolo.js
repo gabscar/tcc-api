85b23c426a98575f99a406b23593a50abbf83b9b
gabscar <srgabsc@gmail.com>
2022-08-20 18:49:21 -0300

    chore: change yolo.js

--- yolo.js
+++ yolo.js
@@ -1,23 +1,26 @@
-const { spawn } = require('child_process');
+const child_process = require('child_process');
 const { dirname } = require('path');
 const appDir = dirname(require.main.filename);
 const script = appDir + '/api/services/yolo_object_detection/yolo.py';
 
 const yolo = (image) => {
+  console.log(script);
   const inputFilePath = appDir + '/images/original_images/' + image;
   const outputFilePath = appDir + '/images//detected_images/' + image;
 
-  const yoloProcess = spawn('python3', [script, inputFilePath, outputFilePath]);
-  yoloProcess.stdout.on('data', (data) =>
-    console.log('python data', data.toString())
-  );
-
   return new Promise((resolve, reject) => {
+    const yoloProcess = child_process.spawn(
+      'python3',
+      [script, inputFilePath, outputFilePath],
+      {
+        detached: true,
+        stdio: 'pipe'
+      }
+    );
+    yoloProcess.stderr.on('data', (data) => console.log(data.toString()));
     yoloProcess.on('close', (code) => {
-      if (code === 0) {
-        resolve(outputFilePath);
-        console.log(code, 'code');
-      } else reject();
+      if (code === 0) resolve(outputFilePath);
+      else reject();
     });
   });
 };
