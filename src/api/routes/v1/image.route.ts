174d3c175bd97d47a5c3bd68ac87cf026915e35c
gabscar <srgabsc@gmail.com>
2022-08-20 18:39:18 -0300

    chore: change image route

--- src/api/routes/v1/image.route.ts
+++ src/api/routes/v1/image.route.ts
@@ -1,18 +1,16 @@
-// import { detectionController } from '../../controllers';
-const yolo = require('../../../../yolo.js');
 import { Router } from 'express';
 const fs = require('fs');
+const yolo = require('../../../../yolo.js');
 const PATH_TO_ORIGINAL_IMAGES = '/images/original_images';
 const route: Router = Router();
 const { dirname } = require('path');
-
 const appDir = dirname(require.main?.filename);
 
-route.get('/images', (_req, res) => {
+route.get('/images', (req, res) => {
   console.log(appDir + '/images/original_images');
   const file = fs.readdir(
     appDir + '/images/original_images',
-    (err: any, images: Buffer) => {
+    (err: any, images: File) => {
       if (err) console.log(err);
       console.log(images);
 
@@ -27,9 +25,9 @@ route.get('/images/:image', (req, res) => {
 });
 
 route.get('/detect/:image', async (req, res) => {
-  // detectionController.detection(req, res);
   try {
     const pathToDetectedImage = await yolo(req.params.image);
+    console.log(pathToDetectedImage);
     res.sendFile(pathToDetectedImage);
   } catch (error) {
     console.log(error);
